33d688ea95504d58470bc90649a22bb564064955
YehyaCh002 <155022478+YehyaCh002@users.noreply.github.com>
2024-06-16 12:48:40 -0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,7 @@
+<header>
+        <h1> welcome to <i>Y phone store </i></h1>
+        <p class="sub-title">
+      We have a wide collection of phone with good prices,Enjoy. 
+   </p>
 
+     </header>
