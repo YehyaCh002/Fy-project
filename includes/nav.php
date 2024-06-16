1088ad8ef4bee363490fbca77d67971007bed653
YehyaCh002 <155022478+YehyaCh002@users.noreply.github.com>
2024-06-16 12:49:04 -0700

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -1,1 +1,13 @@
+<nav>
+    <div class="brand">Y phone</div>
+    <div class="links">
+        <a data-active="index" href="index.php">Home</a>
+        <a data-active="about" href="about.php">About</a>
+        <a data-active="contact" href="contact.php">Contact</a>
+    </div>
+    <div class="auth-links">
+        <a href="login.php">Login</a>
+        <a href="signup.php">Sign Up</a>
+    </div>
+</nav>
 
